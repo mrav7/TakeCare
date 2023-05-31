1e16b4187329bb4bcaa4de06c0cd71690e64dee1
mrav7 <53009333+mrav7@users.noreply.github.com>
2023-05-31 10:27:06 -0400

    Update Register.jsx

--- TakeCare/src/pages/Register.jsx
+++ TakeCare/src/pages/Register.jsx
@@ -4,7 +4,7 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
-import "../components/styles/registerLogin.css";
+import "../components/styles/forms.css";
 import { useForm } from "react-hook-form";
 import {
   MdAccountCircle,
