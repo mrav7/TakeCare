f881ffda8445f3a0324a436a7c065e5647fc7dfc
AlexOttoVicencio <80718046+AlexOttoVicencio@users.noreply.github.com>
2023-06-03 15:30:21 -0400

    Puse la validacion en el login

--- TakeCare/src/pages/Login.jsx
+++ TakeCare/src/pages/Login.jsx
@@ -5,24 +5,55 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import '../components/styles/forms.css'
 import {MdEmail, MdKey} from 'react-icons/md'
-
+import { useForm } from "react-hook-form";
 export default function Register() {
+
+    const {
+        register,
+        watch,
+        getValues,
+        handleSubmit,
+        formState: { errors },
+      } = useForm();
+    
+      const onSubmit = (data) => {
+        console.log(data);
+      };
+
     return (
         <>
             <Container fluid className="bg">            
-            <Form className="form">
+            <Form className="form" onSubmit={handleSubmit(onSubmit)}>
                 <h3 class="_h3">Ingresa</h3>                
                 <Col>
                     <Form.Group className="_mb-3" controlId="emailInput">
                         <Form.Label><MdEmail className="_icon"/></Form.Label>
-                        <Form.Control type="email" placeholder="Ingrese su correo electrónico"></Form.Control>
+                        <Form.Control type="email"
+                        name="email"
+                        {...register("email",{
+                            required:true
+                        }
+                        )}
+                         placeholder="Ingrese su correo electrónico"></Form.Control>
+                         {errors.email && errors.email.type === "required" && (
+                  <p className="errorMsg">Pon tu correo</p>
+                )}
                         <Form.Text></Form.Text>
                     </Form.Group>                
                 </Col>                
                 <Col>
                     <Form.Group className="_mb-3" controlId="passInput">
                         <Form.Label><MdKey className="_icon"/></Form.Label>
-                        <Form.Control type="password" placeholder="Ingrese su contraseña"></Form.Control>
+                        <Form.Control type="password"
+                        name="password"
+                        {...register("password",{
+                            required:true
+                        }
+                        )}
+                         placeholder="Ingrese su contraseña"></Form.Control>
+                         {errors.password && errors.password.type === "required" && (
+                  <p className="errorMsg">Pon tu contraseña</p>
+                )}
                         <Form.Text></Form.Text>
                     </Form.Group>
                 </Col>
