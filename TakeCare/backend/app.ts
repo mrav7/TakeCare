643f4f2225f548cfc0c58fe3d0a17d30009973ad
mrav7 <mandradev@live.cl>
2023-06-24 10:15:24 -0400

    agregados metodos get y post al api rest

--- TakeCare/backend/app.ts
+++ TakeCare/backend/app.ts
@@ -1,23 +1,24 @@
 const express = require('express');
-const mysql = require('mysql2');
-const app = express();
+const mysql = require('mysql');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const jsonParser = bodyParser.json(); 
+var jsonParser = bodyParser.json();
 
-app.use(cors);
+const app = express();
+app.use(cors());
+app.set('port', process.env.PORT || 3000);
 
 // Se establece la conexión con la base de datos MySQL
-const connection = mysql.createConnection( {
-    host: "",
-    user: "",
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
     password: "",
-    port: "",
-    database: "",
+    port: "3306",
+    database: "DB",
 });
 
 // Se establece la conexión con el servidor de la base de datos MySQL
-connection.connect(function(err:any) { 
+connection.connect(function(err:any) {
     if (err) {
         console.error("Error al intentar conectar a la base de datos " + err.stack);
         return;
@@ -26,18 +27,57 @@ connection.connect(function(err:any) {
     }
 });
 
-// Se configura el servidor
-const configuration = {
-    hostname: "",
-    port: "",
-};
+const configuracion = {
+    hostname: "127.0.0.1",
+    port: 3000,
+}
 
-// POST
+app.listen(app.get('port'), () => {
+    console.log("Servidor funcionando en el puerto", app.get('port'));
+});
 
-// GET 
+app.post("/createUser", jsonParser,(req:any, res:any) => {
+    let firstname = req.body.firstname;
+    let lastname = req.body.lastname;
+    let email = req.body.email;
+    let profession = req.body.profession;
+    let password = req.body.password;
+    let isAdmin = req.body.isAdmin;
+    let user = {
+        firstname: firstname,
+        lastname: lastname,
+        email: email,
+        profession: profession,
+        password: password,
+        isAdmin: isAdmin
+    }
 
-// DELETE
+    connection.query("INSERT INTO Users SET ?", user, (error:any, results:any, fields:any) => {
+        if (error) {
+            console.error("Error creating user: ", error);
+            res.status(500).send("Error creating user");
+            return;
+        }
+        
+        console.log("User created successfully!");
+        res.status(200).send("User created successfully");
+    });
+});
 
-// UPDATE
+app.get('/users', (req:any, res:any) => {
+    connection.query("SELECT * FROM Users", (error:any, results:any) => {
+        if (error) {
+            console.error(error);
+            res.status(500).send("No se hace ha podido realizar SELECT al servidor");
+        } else {
+            const response = {
+                status: "Exito",
+                message: "Se ha realizado SELECT exitosamente",
+                data: results
+            }
+            res.status(200).json(response);
+        }
+    })
+})
 
 
--- TakeCare/backend/dist/app.js
+++ TakeCare/backend/dist/app.js
@@ -1,18 +1,19 @@
 "use strict";
 const express = require('express');
-const mysql = require('mysql2');
-const app = express();
+const mysql = require('mysql');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const jsonParser = bodyParser.json();
-app.use(cors);
+var jsonParser = bodyParser.json();
+const app = express();
+app.use(cors());
+app.set('port', process.env.PORT || 3000);
 // Se establece la conexión con la base de datos MySQL
 const connection = mysql.createConnection({
-    host: "",
-    user: "",
+    host: "localhost",
+    user: "root",
     password: "",
-    port: "",
-    database: "",
+    port: "3306",
+    database: "DB",
 });
 // Se establece la conexión con el servidor de la base de datos MySQL
 connection.connect(function (err) {
@@ -24,12 +25,51 @@ connection.connect(function (err) {
         console.log("Conexión establecida " + connection.threadId);
     }
 });
-// Se configura el servidor
-const configuration = {
-    hostname: "",
-    port: "",
+const configuracion = {
+    hostname: "127.0.0.1",
+    port: 3000,
 };
-// POST
-// GET 
-// DELETE
-// UPDATE
+app.listen(app.get('port'), () => {
+    console.log("Servidor funcionando en el puerto", app.get('port'));
+});
+app.post("/createUser", jsonParser, (req, res) => {
+    let firstname = req.body.firstname;
+    let lastname = req.body.lastname;
+    let email = req.body.email;
+    let profession = req.body.profession;
+    let password = req.body.password;
+    let isAdmin = req.body.isAdmin;
+    let user = {
+        firstname: firstname,
+        lastname: lastname,
+        email: email,
+        profession: profession,
+        password: password,
+        isAdmin: isAdmin
+    };
+    connection.query("INSERT INTO Users SET ?", user, (error, results, fields) => {
+        if (error) {
+            console.error("Error creating user: ", error);
+            res.status(500).send("Error creating user");
+            return;
+        }
+        console.log("User created successfully!");
+        res.status(200).send("User created successfully");
+    });
+});
+app.get('/users', (req, res) => {
+    connection.query("SELECT * FROM Users", (error, results) => {
+        if (error) {
+            console.error(error);
+            res.status(500).send("No se hace ha podido realizar SELECT al servidor");
+        }
+        else {
+            const response = {
+                status: "Exito",
+                message: "Se ha realizado SELECT exitosamente",
+                data: results
+            };
+            res.status(200).json(response);
+        }
+    });
+});
